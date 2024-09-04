37e1187c97d95f16c0712ba16f24108b135ac072
injae-348 <ant0593@naver.com>
2024-09-05 00:12:25 +0900

    feat: 테스트용 CORS 설정

---
+++ src/main/java/lab/dev/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package lab.dev.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
+    }
+}
