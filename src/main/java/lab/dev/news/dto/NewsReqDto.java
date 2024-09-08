4ba3faff62603c7838afb771b741f2d19919ef41
injae-348 <ant0593@naver.com>
2024-09-08 20:01:22 +0900

    feat: Admin 뉴스 생성 페이지 추가, 종료 시 파일 삭제 로직 추가

--- src/main/java/lab/dev/news/dto/NewsReqDto.java
+++ src/main/java/lab/dev/news/dto/NewsReqDto.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lab.dev.news.domain.News;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -13,6 +15,7 @@ import java.util.List;
 public record NewsReqDto(
 
         @NotNull(message = "날짜를 입력해주세요.")
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime date,
 
         @NotNull(message = "활동을 입력해주세요.")
--- src/main/java/lab/dev/news/dto/NewsResDto.java
+++ src/main/java/lab/dev/news/dto/NewsResDto.java
@@ -1,7 +1,9 @@
 package lab.dev.news.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lab.dev.file.domain.UploadFile;
 import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,7 @@ import java.util.List;
 @Builder
 public record NewsResDto(
         Long id,
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime date,
         String activity,
         String content,
